8cbaada2af618889a9ff6421ce9ef79d54e6dc43
Namli <tilmanherz03@gmail.com>
2022-05-13 10:43:14 +0200

    Updates

---
+++ partials/HeroHome.jsx
@@ -0,0 +1,116 @@
+import React, { useState } from 'react';
+import Modal from '../utils/Modal';
+
+import HeroImage from '../images/hero-image.png';
+
+function HeroHome() {
+
+  const [videoModalOpen, setVideoModalOpen] = useState(false);
+
+  return (
+    <section className="relative">
+
+      {/* Illustration behind hero content */}
+      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none" aria-hidden="true">
+        <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
+              <stop stopColor="#FFF" offset="0%" />
+              <stop stopColor="#EAEAEA" offset="77.402%" />
+              <stop stopColor="#DFDFDF" offset="100%" />
+            </linearGradient>
+          </defs>
+          <g fill="url(#illustration-01)" fillRule="evenodd">
+            <circle cx="1232" cy="128" r="128" />
+            <circle cx="155" cy="443" r="64" />
+          </g>
+        </svg>
+      </div>
+
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+
+        {/* Hero content */}
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+
+          {/* Section header */}
+          <div className="text-center pb-12 md:pb-16">
+            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">We Run The <span className="bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-red-600">World</span></h1>
+            <div className="max-w-3xl mx-auto">
+              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">
+                Zusammen laufen wir einmal durch China! Seid gespannt auf Artikel, Podcasts und spannende Challenges entlang des Wegs.
+                </p>
+              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
+                <div>
+                  <a className="btn text-white bg-red-600 hover:bg-red-700 w-full mb-4 sm:w-auto sm:mb-0" target="_blank" href="https://www.google.com/maps/dir/Harbin,+Heilongjiang,+China/Shenyang,+Liaoning,+China/Peking,+China/Qingdao,+Shandong,+China/Shanghai,+China/Hangzhou,+Zhejiang,+China/Shenzhen,+Guangdong,+China/@32.8660599,108.0537403,5z/data=!4m44!4m43!1m5!1m1!1s0x5e4364f8a6641461:0x5e7c92735aa02cd5!2m2!1d126.6424173!2d45.7567307!1m5!1m1!1s0x5e253c413a1334b7:0x5499e4f0ce929166!2m2!1d123.4329681!2d41.804778!1m5!1m1!1s0x35f05296e7142cb9:0xb9625620af0fa98a!2m2!1d116.407395!2d39.904211!1m5!1m1!1s0x35960fd582f8f06b:0x614d82fa614cf2f3!2m2!1d120.38299!2d36.0662299!1m5!1m1!1s0x35b27040b1f53c33:0x295129423c364a1!2m2!1d121.473701!2d31.230416!1m5!1m1!1s0x344bb629439aaa99:0xa7bfd183824de83a!2m2!1d120.15515!2d30.2741499!1m5!1m1!1s0x3403f408d0e15291:0xfdee550db79280c9!2m2!1d114.05956!2d22.5428599!3e0?hl=de">
+                    Weg ansehen
+                    </a>
+                </div>
+                <div>
+                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">
+                    Mitmachen
+                    </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Hero image */}
+          <div>
+            <div className="relative flex justify-center mb-8" data-aos="zoom-y-out" data-aos-delay="450">
+              <div className="flex flex-col justify-center">
+                <img className="mx-auto" src={HeroImage} width="768" height="432" alt="Hero" />
+                <svg className="absolute inset-0 max-w-full mx-auto md:max-w-none h-auto" width="768" height="432" viewBox="0 0 768 432" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                  <defs>
+                    <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="hero-ill-a">
+                      <stop stopColor="#FFF" offset="0%" />
+                      <stop stopColor="#EAEAEA" offset="77.402%" />
+                      <stop stopColor="#DFDFDF" offset="100%" />
+                    </linearGradient>
+                    <linearGradient x1="50%" y1="0%" x2="50%" y2="99.24%" id="hero-ill-b">
+                      <stop stopColor="#FFF" offset="0%" />
+                      <stop stopColor="#EAEAEA" offset="48.57%" />
+                      <stop stopColor="#DFDFDF" stopOpacity="0" offset="100%" />
+                    </linearGradient>
+                    <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="hero-ill-e">
+                      <stop stopColor="#4FD1C5" offset="0%" />
+                      <stop stopColor="#81E6D9" offset="25.871%" />
+                      <stop stopColor="#338CF5" offset="100%" />
+                    </radialGradient>
+                    <circle id="hero-ill-d" cx="384" cy="216" r="64" />
+                  </defs>
+                  <g fill="none" fillRule="evenodd">
+                    <circle fillOpacity=".04" fill="url(#hero-ill-a)" cx="384" cy="216" r="128" />
+                    <circle fillOpacity=".16" fill="url(#hero-ill-b)" cx="384" cy="216" r="96" />
+                    <g fillRule="nonzero">
+                      <use fill="#000" xlinkHref="#hero-ill-d" />
+                      <use fill="url(#hero-ill-e)" xlinkHref="#hero-ill-d" />
+                    </g>
+                  </g>
+                </svg>
+              </div>
+              {/* <button className="absolute top-full flex items-center transform -translate-y-1/2 bg-white rounded-full font-medium group p-4 shadow-lg" onClick={(e) => { e.preventDefault(); e.stopPropagation(); setVideoModalOpen(true); }} aria-controls="modal">
+                <svg className="w-6 h-6 fill-current text-gray-400 group-hover:text-blue-600 flex-shrink-0" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
+                  <path d="M10 17l6-5-6-5z" />
+                </svg>
+                <span className="ml-3">Watch the full video (2 min)</span>
+              </button> */}
+            </div>
+
+            {/* Modal */}
+            {/* <Modal id="modal" ariaLabel="modal-headline" show={videoModalOpen} handleClose={() => setVideoModalOpen(false)}>
+              <div className="relative pb-9/16">
+                <iframe className="absolute w-full h-full" src="https://player.vimeo.com/video/174002812" title="Video" allowFullScreen></iframe>
+              </div>
+            </Modal> */}
+
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+  );
+}
+
+export default HeroHome;
